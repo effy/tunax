6a005cdfc20733823e02b9d1d1c1acf23b4b9162
Toshio Koide <tkoide3@gmail.com>
2010-09-18 23:49:51 +0900

    created openflow header class

---
+++ header_sample.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby
+
+require 'openflow'
+
+h1 = OFPHeader.new
+h1.set(0, OFPT_ECHO_REQUEST, 8, 12345) #create header
+puts h1.msg.unpack('H*') #get header(binary)
+
+h2 = OFPHeader.new
+h2.msg = h1.msg #set header(binary)
+p h2.version, h2.type, h2.len, h2.xid #obtain each member of the header
--- openflow.rb
+++ openflow.rb
@@ -37,3 +37,36 @@ OFPT_BARRIER_REPLY = 19       # Controller/switch message
 OFPT_QUEUE_GET_CONFIG_REQUEST = 20  # Controller/switch message
 OFPT_QUEUE_GET_CONFIG_REPLY = 21    # Controller/switch message
 
+## opf_header ##
+
+class OFPHeader
+    def set(*args)
+        @version, @type, @len, @xid = args
+        @msg = args.pack("CCnN")
+    end
+
+    def msg=(value)
+    	@msg = value[0,8]
+	@version, @type, @len, @xid = @msg.unpack("CCnN")
+    end
+
+    def msg
+        @msg
+    end
+
+    def version
+        @version
+    end
+
+    def type
+    	@type
+    end
+
+    def len
+    	@len
+    end
+
+    def xid
+	@xid
+    end
+end
