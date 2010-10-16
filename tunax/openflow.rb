f279b13ae85f51f2e5042bd69e2604080d1bfe81
Toshio Koide <tkoide3@gmail.com>
2010-10-17 00:33:58 +0900

    created OFPErrors class and modified pack method of each struct classes

--- tunax/openflow.rb
+++ tunax/openflow.rb
@@ -17,6 +17,9 @@
 #
 
 OFP_VERSION = 0x01
+OFP_MAX_ETH_ALEN = 6
+OFP_MAX_PORT_NAME_LEN = 16
+
 
 # OpenFlow Message Types
 #
@@ -58,6 +61,70 @@ class OFPTypes
     OFPT_QUEUE_GET_CONFIG_REPLY = 21    # Controller/switch message
 end
 
+class OFPErrors
+    # ofp_error_type
+    OFPET_HELLO_FAILED = 0     # Hello protocol failed.
+    OFPET_BAD_REQUEST = 1      # Request was not understood.
+    OFPET_BAD_ACTION = 2       # Error in action description.
+    OFPET_FLOW_MOD_FAILED = 3  # Problem modifying flow entry.
+    OFPET_PORT_MOD_FAILED = 4  # Port mod request failed.
+    OFPET_QUEUE_OP_FAILED = 5  # Queue operation failed.
+
+    # ofp_hello_failed_code
+    OFPHFC_INCOMPATIBLE = 0    # No compatible version.
+    OFPHFC_EPERM = 1           # Permissions error.
+
+    # ofp_bad_request_code
+    OFPBRC_BAD_VERSION = 0     # ofp_header.version not supported.
+    OFPBRC_BAD_TYPE = 1        # ofp_header.type not supported.
+    OFPBRC_BAD_STAT = 2        # ofp_stats_request.type not supported.
+    OFPBRC_BAD_VENDOR = 3      # Vendor not supported (in ofp_vendor_header or ofp_stats_request or ofp_stats_reply).
+    OFPBRC_BAD_SUBTYPE = 4     # Vendor subtype not supported.
+    OFPBRC_EPERM = 5           # Permissions error.
+    OFPBRC_BAD_LEN = 6         # Wrong request length for type.
+    OFPBRC_BUFFER_EMPTY = 7    # Specified buffer has already been used.
+    OFPBRC_BUFFER_UNKNOWN = 8  # Specified buffer does not exist.
+
+    # ofp_bad_request_code
+    OFPBRC_BAD_VERSION = 0
+    OFPBRC_BAD_TYPE = 1
+    OFPBRC_BAD_STAT = 2
+    OFPBRC_BAD_VENDOR = 3
+    OFPBRC_BAD_SUBTYPE = 4
+    OFPBRC_EPERM = 5
+    OFPBRC_BAD_LEN = 6
+    OFPBRC_BUFFER_EMPTY = 7
+    OFPBRC_BUFFER_UNKNOWN = 8
+
+    # ofp_bad_action_code
+    OFPBAC_BAD_TYPE = 0
+    OFPBAC_BAD_LEN = 1
+    OFPBAC_BAD_VENDOR = 2
+    OFPBAC_BAD_VENDOR_TYPE = 3
+    OFPBAC_BAD_OUT_PORT = 4
+    OFPBAC_BAD_ARGUMENT = 5
+    OFPBAC_EPERM = 6
+    OFPBAC_TOO_MANY = 7
+    OFPBAC_BAD_QUEUE = 8
+
+    # ofp_flow_mod_failed_code
+    OFPFMFC_ALL_TABLES_FULL = 0
+    OFPFMFC_OVERLAP = 1
+    OFPFMFC_EPERM = 2
+    OFPFMFC_BAD_EMERG_TIMEOUT = 3
+    OFPFMFC_BAD_COMMAND = 4
+    OFPFMFC_UNSUPPORTED = 5
+    
+    # ofp_port_mod_failed_code
+    OFPPMFC_BAD_PORT = 0     # Specified port does not exist.
+    OFPPMFC_BAD_HW_ADDR = 1  # Specified hardware address is wrong.
+
+    # ofp_queue_op_failed_code
+    OFPQOFC_BAD_PORT = 0
+    OFPQOFC_BAD_QUEUE = 1
+    OFPQOFC_EPERM = 2
+end
+
 # Struct of Openflow protocol header
 #
 # To create a new instance, use new(version, type, length, xid) or create_from(data)
@@ -71,8 +138,7 @@ class OFPHeader < Struct.new(:version, :type, :length, :xid)
 
     # returns the binary data of this OpenFlow header
     def pack
-	@raw = [self.version, self.type, self.length, self.xid].pack("CCnN") if @raw == nil
-	@raw
+	self.to_a.pack("CCnN")
     end
 
     # :version
@@ -111,8 +177,7 @@ class OFPMessage < Struct.new(:header, :payload)
 
     # returns the binary data of this OpenFlow message
     def pack
-	@raw = self.header.pack + self.payload if @raw == nil
-	@raw
+	self.header.pack + self.payload
     end
 
     # returns length of the message
@@ -140,7 +205,40 @@ class OFPSwitchFeatures < Struct.new(:header, :datapath_id, :n_buffers, :n_table
 	fields = message.payload.unpack("NNNCxxxNN")
 	fields[1] |= fields[0] << 32
 	fields.shift
-	#TODO proc ports
+
+	#ports
+	ports = []
+	for i in 1..((message.header.length - 8 - 24) / 48) #8:ofp_header, 24:ofp_switch_features, 48:ofp_phy_port
+	    ofp_phy_port_data = (message.payload[24+(i-1)*48, 48])
+	    ports.push(OFPPhyPort.create_from(ofp_phy_port_data))
+	end
+	fields.push(ports)
+
+	#create
 	new(message.header, *fields)
+    end
+
+    def pack
+	self.header.pack + self.payload
+    end
+
+    def payload
+	ret = self.to_a.pack("NNNCxxxNN")
+	for i in 0..ports.length-1
+	    ret += ports[i].pack
+	end
+	ret
+    end
+end
+
+class OFPPhyPort < Struct.new(:port_no, :hw_addr, :name, :config, :state, :curr, :advertised, :supported, :peer)
+    # creates a new instance from binary data of OpenFlow physical port
+    def self.create_from(data)
+	new(*data.unpack("na6Z16NNNNNN"))
+    end
+
+    # returns the binary data of this OpenFlow physical port
+    def pack
+	self.to_a.pack("na6Z16NNNNNN")
     end
 end
